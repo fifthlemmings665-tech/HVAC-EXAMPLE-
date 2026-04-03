e216bca2daea2e8c1b261a290017c41fd07275b3
fifthlemmings665-tech <fifth.lemmings665@gmail.com>
2026-04-03 17:43:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function scrollToSection() {
-    document.getElementById("cta").scrollIntoView({ behavior: "smooth" });
-}
 
-function submitForm(e) {
-    e.preventDefault();
-    alert("Message sent!");
-}
